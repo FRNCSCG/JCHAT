27e4454eab2d7688c394e48f302ecd4e0fbdaa28
FRNCSCG <fran_s99@live.com>
2023-03-28 00:20:30 -0300

    yes

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -58,6 +58,64 @@ export default function ChatList() {
            </div>
           </div>
         ))}
+        {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat, id) => (
+          //chat[0] is the element id, chat[1] is the chat data
+          <div 
+          key={chat[0]}
+          id={chat[0]} 
+          className={`chat-item ${(selectedStyle===chat[0] && state.selected===true) && "selected"} `}
+          onClick={()=>handleSelect(chat[1].userInfo, chat[0])}>
+           <img className='chat-img' src={chat[1].userInfo.photoURL} alt=''/>
+           <div className='chat-info '>
+             <p className='chat-title textbreak'>{chat[1].userInfo.displayName}</p>
+             <p className='last-msg textbreak'>{chat[1].lastMessage?.text}</p>
+           </div>
+          </div>
+        ))}
+        {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat, id) => (
+          //chat[0] is the element id, chat[1] is the chat data
+          <div 
+          key={chat[0]}
+          id={chat[0]} 
+          className={`chat-item ${(selectedStyle===chat[0] && state.selected===true) && "selected"} `}
+          onClick={()=>handleSelect(chat[1].userInfo, chat[0])}>
+           <img className='chat-img' src={chat[1].userInfo.photoURL} alt=''/>
+           <div className='chat-info '>
+             <p className='chat-title textbreak'>{chat[1].userInfo.displayName}</p>
+             <p className='last-msg textbreak'>{chat[1].lastMessage?.text}</p>
+           </div>
+          </div>
+        ))}
+{Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat, id) => (
+          //chat[0] is the element id, chat[1] is the chat data
+          <div 
+          key={chat[0]}
+          id={chat[0]} 
+          className={`chat-item ${(selectedStyle===chat[0] && state.selected===true) && "selected"} `}
+          onClick={()=>handleSelect(chat[1].userInfo, chat[0])}>
+           <img className='chat-img' src={chat[1].userInfo.photoURL} alt=''/>
+           <div className='chat-info '>
+             <p className='chat-title textbreak'>{chat[1].userInfo.displayName}</p>
+             <p className='last-msg textbreak'>{chat[1].lastMessage?.text}</p>
+           </div>
+          </div>
+        ))}
+{Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat, id) => (
+          //chat[0] is the element id, chat[1] is the chat data
+          <div 
+          key={chat[0]}
+          id={chat[0]} 
+          className={`chat-item ${(selectedStyle===chat[0] && state.selected===true) && "selected"} `}
+          onClick={()=>handleSelect(chat[1].userInfo, chat[0])}>
+           <img className='chat-img' src={chat[1].userInfo.photoURL} alt=''/>
+           <div className='chat-info '>
+             <p className='chat-title textbreak'>{chat[1].userInfo.displayName}</p>
+             <p className='last-msg textbreak'>{chat[1].lastMessage?.text}</p>
+           </div>
+          </div>
+        ))}
+
+
 
     </div>
   )
