7b138086618ae789d461000b5a60e5a3f5f78875
FRNCSCG <fran_s99@live.com>
2023-05-22 18:00:41 -0300

    yeahf

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import { Divider, IconButton, TextField } from '@mui/material'
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 import { collection, where, getDocs, query, getDoc, doc, setDoc, updateDoc, serverTimestamp } from 'firebase/firestore';
@@ -9,10 +9,22 @@ export default function Search() {
   const [username, setUsername] = useState("")
   const [user, setUser] = useState(null)
   const [err, setErr] = useState(false)
+  const [searching, setSearching] = useState(false)
+  const [entered, setEntered] = useState(false)
+
+
+  useEffect(() => {
+    console.log(searching)
+    // user ? setSearching(true) : setSearching(false)
+
+  }, [searching, user])
+  
   
   const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
+    setUser("")
+    setEntered(true)
     const q = query (
       collection (db, "users"),
       where ("displayName", "==", username)
@@ -26,10 +38,6 @@ export default function Search() {
       setErr(true)
     }
   }
-
-  const handleKey = (e) => {
-    e.code === "Enter" && handleSearch();
-  };
   
   const handleSelect = async () => {
     //check whether the group(chats in firestore) exists, if not create
@@ -72,14 +80,18 @@ export default function Search() {
   const handleClearClick = () => {
     setUsername("")
     setUser("")
+    setSearching(false)
+    setEntered(false)
   }
 
   return (
     <div className='search'>
       <TextField
         type="text"
-        onKeyDown={handleKey}
+        onKeyPress={(event) => event.key === 'Enter' && handleSearch()}
         value={username}
+        onFocus={() => setSearching(true)}
+        // onBlur={() => setSearching(false)}
         onChange={(e) => setUsername(e.target.value)}
         sx={{width: "100%", backgroundColor:''}} 
         size='small' variant="filled"  label='search users, chats...'
@@ -88,12 +100,13 @@ export default function Search() {
         }}
         InputProps={{
           className:"textfield-input",
-          endAdornment:(<IconButton onClick={handleClearClick} sx={{visibility: username ? "visible" : "hidden", color:"aliceblue"}}><ClearIcon/></IconButton>)
+          endAdornment:(<IconButton onClick={handleClearClick} sx={{visibility: searching ? "visible" : "hidden", color:"aliceblue"}}><ClearIcon/></IconButton>)
         }}
 
       />
-      <div style={{height:'auto', width:'100%', backgroundColor:'darkblue'}}>
-        {(user) ?  (
+      {/* <div style={{height:'auto', width:'100%', backgroundColor:'darkblue'}}> */}
+      <div className={`search-results ${searching && "searching" }`}>
+        {(user) ? (
           <div className="chat-item" onClick={handleSelect}>
             <img src={user.photoURL} className="chat-img" alt="" />
             <div className='chat-info'>
@@ -101,10 +114,13 @@ export default function Search() {
             </div>
             <Divider variant='middle'/>
           </div>
-          
         )
         :
-          <></>
+             (entered) ? 
+              (<div style={{ textAlign:'center', padding:'5%'}}><p>no users found!</p></div>)
+              :
+              (<div style={{ textAlign:'center', padding:'5%'}}><p>Search by username (search for "frank" to chat with me!)</p></div>)
+            
         }
       </div>
     </div>
